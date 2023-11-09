4aa3df0013f6d15c84c5c4e3828532d41bfc8178
Andrei Basdereff <abasdere@bess-f3r3s3.clusters.42paris.fr>
2023-11-09 16:42:39 +0100

    done truelly

--- ft_putun_nbr_base.c
+++ ft_putun_nbr_base.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:24:25 by abasdere          #+#    #+#             */
-/*   Updated: 2023/11/09 16:40:58 by abasdere         ###   ########.fr       */
+/*   Updated: 2023/11/09 16:42:08 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	ft_nb_recu(unsigned long n, char *base, size_t b_size, int *len)
 {
 	*len = *len + 1;
 	if (n / b_size)
-		ft_putnbr_base_recu(n / b_size, base, b_size, len);
+		ft_nb_recu(n / b_size, base, b_size, len);
 	n = base[n % b_size];
 	write(1, &n, 1);
 }
